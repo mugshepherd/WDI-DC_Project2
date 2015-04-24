1c08ee525cac934559be2888fa6e689e191d1ca0
Nick Bumbarger <nbumbarger@gmail.com>
2015-04-24 08:44:10 -0400

    added article caching, addded multiple news sections accessible through site navigation bar)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,9 @@ class User < ActiveRecord::Base
   has_many :favorited_articles
   has_many :articles, through: :favorited_articles
 
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :path => 'accounts'
 
-  root 'articles#index'
+  root 'articles#news'
+  get  'articles/news', 'articles/sports', 'articles/life', 'articles/money',
+       'articles/tech', 'articles/travel', 'articles/opinion', 'articles/weather'
   resources :users
   resources :articles
 
